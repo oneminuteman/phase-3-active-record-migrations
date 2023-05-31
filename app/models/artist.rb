fbba78a6d300664737c85c0c56fb9151ef542221
Victor <victor.ireri@student.moringaschool.com>
2023-05-31 09:55:59 +0300

    labs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,15 @@
 # add the Artist class here
+# db/migrate/20210716095220_create_artists.rb
+class CreateArtists < ActiveRecord::Migration[6.1]
+    def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+  
+        t.timestamps
+      end
+    end
+  end
+  
